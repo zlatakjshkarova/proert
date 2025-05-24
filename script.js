7c18f5b24d9c535c42c96d8dec23d7b4e0c58e3d
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2025-05-24 12:57:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,26 +7,19 @@ const list = document.querySelector('#list');
 const model = document.querySelector("#model");
 const cenle = document.querySelector("#cenle");
 const delButModel = document.querySelector("#del");
+const table = document.querySelectorAll('#table');
+const total = document.querySelector('#total');
+let td;
 
+let sum = 0;
 
 
 
 myBtn4.addEventListener('click', Task)
 
 
-
-
-
-
-
-
-
-
-
-
-
 function Task() {
-    if (nameInput.value !== "") {
+    if (nameInput.value !== "" && nameInput2.value !== "" && nameInput3 !== "") {
 
         const newItem = document.createElement("li");
 
@@ -34,21 +27,31 @@ function Task() {
 
         const deleteButton = document.createElement('button')
         deleteButton.textContent = 'удалить'
-        newItem.innerHTML = `<span>${nameInput.value}</span>`;
+        newItem.innerHTML = `
+        <h1 class="expense expense-title">${nameInput.value}</h1>
+        <p class="expense expense-category">${nameInput3.value}</p>
+        <p class="expense expense-category">${nameInput2.value}</p>
+        `;
+
+        sum = sum + +nameInput2.value;
+        console.log(sum);
+        total.textContent = ++sum;
+
         newItem.appendChild(deleteButton)
         list.appendChild(newItem);
 
         nameInput.value = '';
 
 
-
-
-
         deleteButton.addEventListener('click', () => {
             currentItem = newItem;
 
-            model.style.display = 'flex';
 
+            if (currentItem) {
+                list.removeChild(currentItem);
+                model.style.display = 'none';
+                currentItem = null;
+            }
         })
 
 
@@ -59,144 +62,98 @@ function Task() {
 
 }
 
-delButModel.addEventListener('click', () => {
-    if (currentItem) {
-        list.removeChild(currentItem);
-        model.style.display = 'none';
-        currentItem = null;
-    }
 
-})
 
 
 
 
-cenle.addEventListener('click', () => {
 
-    model.style.display = 'none';
-    currentItem = null;
 
 
-})
-myBtn4.addEventListener('click', Task2)
+// myBtn4.addEventListener('click', Task2)
 
-function Task2() {
-    if (nameInput2.value !== "") {
+// function Task2() {
+//     if (nameInput2.value !== "") {
 
-        const newItem = document.createElement("li");
+//         const newItem = document.createElement("li");
 
-        newItem.classList.add('item');
+//         newItem.classList.add('item');
 
-        const deleteButton = document.createElement('button')
-        deleteButton.textContent = 'удалить'
-        newItem.innerHTML = `<span>${nameInput2.value}</span>`;
-        newItem.appendChild(deleteButton)
-        list.appendChild(newItem);
+//         const deleteButton = document.createElement('button')
+//         deleteButton.textContent = 'удалить'
+//         newItem.innerHTML = `<span>${nameInput2.value}</span>`;
+//         newItem.appendChild(deleteButton)
+//         list.appendChild(newItem);
 
-        nameInput2.value = '';
+//         nameInput2.value = '';
 
 
 
 
 
-        deleteButton.addEventListener('click', () => {
-            currentItem = newItem;
+//         deleteButton.addEventListener('click', () => {
+//             currentItem = newItem;
 
-            model.style.display = 'flex';
+//             if (currentItem) {
+//                 list.removeChild(currentItem);
+//                 model.style.display = 'none';
+//                 currentItem = null;
+//             }
+//         })
 
-        })
 
+//     } else {
+//         alert('Заполните поле');
+//     }
 
-    } else {
-        alert('Заполните поле');
-    }
 
+// }
 
-}
 
-delButModel.addEventListener('click', () => {
-    if (currentItem) {
-        list.removeChild(currentItem);
-        model.style.display = 'none';
-        currentItem = null;
-    }
 
-})
 
 
 
 
-cenle.addEventListener('click', () => {
 
-    model.style.display = 'none';
-    currentItem = null;
+// myBtn4.addEventListener('click', Task3)
 
 
-})
 
+// function Task3() {
+//     if (nameInput3.value !== "") {
 
+//         const newItem = document.createElement("li");
 
+//         newItem.classList.add('item');
 
+//         const deleteButton = document.createElement('button')
+//         deleteButton.textContent = 'удалить'
+//         newItem.innerHTML = `<span>${nameInput3.value}</span>`;
+//         newItem.appendChild(deleteButton)
+//         list.appendChild(newItem);
 
+//         nameInput3.value = '';
 
 
 
 
-myBtn4.addEventListener('click', Task3)
 
+//         deleteButton.addEventListener('click', () => {
+//             currentItem = newItem;
 
+//             if (currentItem) {
+//                 list.removeChild(currentItem);
+//                 model.style.display = 'none';
+//                 currentItem = null;
+//             }
+//         })
 
-function Task3() {
-    if (nameInput3.value !== "") {
 
-        const newItem = document.createElement("li");
+//     } else {
+//         alert('Заполните поле');
+//     }
 
-        newItem.classList.add('item');
 
-        const deleteButton = document.createElement('button')
-        deleteButton.textContent = 'удалить'
-        newItem.innerHTML = `<span>${nameInput3.value}</span>`;
-        newItem.appendChild(deleteButton)
-        list.appendChild(newItem);
-
-        nameInput3.value = '';
-
-
-
-
-
-        deleteButton.addEventListener('click', () => {
-            currentItem = newItem;
-
-            model.style.display = 'flex';
-
-        })
-
-
-    } else {
-        alert('Заполните поле');
-    }
-
-
-}
-
-delButModel.addEventListener('click', () => {
-    if (currentItem) {
-        list.removeChild(currentItem);
-        model.style.display = 'none';
-        currentItem = null;
-    }
-
-})
-
-
-
-
-cenle.addEventListener('click', () => {
-
-    model.style.display = 'none';
-    currentItem = null;
-
-
-})
+// }
 
